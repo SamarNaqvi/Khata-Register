85e3200e282b39e039374eedc25175343e8b7334
Askar Gillani <111361910+askargillani@users.noreply.github.com>
2022-12-02 20:20:02 +0500

    Adding login, signup, Profile

---
+++ app/src/main/java/com/example/khataregister/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.khataregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    android.widget.Button submitButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        submitButton = (android.widget.Button)findViewById(R.id.submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("yes");
+                String em = email.getText().toString();
+                Intent intent=new Intent(LoginActivity.this,MainProfile.class) ;
+                intent.putExtra("email",em);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+}
